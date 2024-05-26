8838588855bb4fe62dc0b9fd79ec7165de58ac2b
Nilesh Baban Binnar <102431318+nilu0611@users.noreply.github.com>
2024-05-26 23:30:41 +0530

    Create worldApp.js

---
+++ worldApp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import InformativePage from './InformativePage';
+
+function App() {
+  return (
+    <div className="App">
+      <InformativePage />
+    </div>
+  );
+}
+
+export default App;
